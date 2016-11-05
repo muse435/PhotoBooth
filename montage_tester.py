6c0e30b7aea466860417151cd8b41d6b5b9e4a93
muse435 <muse435@gmail.com>
2016-11-05 11:14:04 -0400

    Update montage_tester.py

--- montage_tester.py
+++ montage_tester.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python
 
-import time, os, subprocess, sys
+#this is a simple script to test different photostrip montagaes
+
+import subprocess, sys
 
 while True:
-  if (1):
-    snap = 0
     print("please wait while we build your photo strip")
-
     subprocess.call("sudo /home/pi/scripts/photobooth_tester/assemble_and_print_hi_res", shell=True)
     sys.exit("End of Test")
     
