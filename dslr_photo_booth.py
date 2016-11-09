28ae79bdf0274806cb8c587041b38c40a1d4d757
muse435 <muse435@gmail.com>
2016-11-09 18:55:56 -0500

    Update dslr_photo_booth.py

--- dslr_photo_booth.py
+++ dslr_photo_booth.py
@@ -2,12 +2,16 @@
 import RPi.GPIO as GPIO, sys, time, os, subprocess, pygame
 
 GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
 
-SWITCH = 26 #button to initiate photos
+# GPIO
+# Swithes
+SWITCH = 26 # button to initiate photos
 GPIO.setup(SWITCH,GPIO.IN, pull_up_down=GPIO.PUD_UP)
-#RESET = 25 #Button to reset
-#GPIO.setup(RESET, GPIO.IN)
+# RESET = 25 #Button to reset
+# GPIO.setup(RESET, GPIO.IN)
 
+# LEDs
 POSE_LED = 12 #Red
 GPIO.setup(POSE_LED, GPIO.OUT)
 GPIO.output(POSE_LED, False)
@@ -19,7 +23,7 @@ GPIO.setup(READY_LED, GPIO.OUT)
 GPIO.output(READY_LED, True)
 
 
-#Variables
+# Variables
 dir = "/home/pi/PB_archive/"
 width = 768
 height = 1024
@@ -27,7 +31,7 @@ wid2 = width/2
 high2 = height/2
 poser = ["First Pose", "Second Pose", "Third Pose", "Last Pose!"]
 
-#pygame
+# pygame
 white = pygame.Color(255,255,255)
 black = pygame.Color(0,0,0)
 pygame.init()
@@ -45,13 +49,15 @@ def GetDirectory(list):
     for file in list:
         print file
 
+# TODO: Work on screen layout
 def BigNumber(number):
     backGroundCenterSurface = pygame.Surface((width,height))
     backGroundCenterSurface.fill(black)
     screen.blit(backGroundCenterSurface,(0,0))
-    backGroundCenterSurface.setalpha(25)
+    backGroundCenterSurface.set_alpha(25)
     screen.blit(bigfont.render(number, 1, white),(200,0))
-    screen.display.update()
+    pygame.display.update()
+
 
 def DrawCenterMessage(message,x,y,ss):
     ww = 0.3*ss*len(message)
@@ -64,6 +70,7 @@ def DrawCenterMessage(message,x,y,ss):
     screen.blit(backGroundCenterSurface,(0,0))
     screen.blit(megafont.render(message, 1, white),(xx+10,yy+10))
     pygame.display.update()
+
 
 def DrawCenterMessage2(message,x,y,ss):
     megafont = pygame.font.SysFont("freeserif",ss,bold = 1)
@@ -82,7 +89,7 @@ def terminate(Terminated):
     pygame.quit()
     sys.exit(Terminated)
 
-#TODO: Impliment
+# TODO: Impliment
 def checkForKeyPress(): 
     for event in pygame.event.get():
         if event.key == K_ESCAPE:
@@ -94,32 +101,61 @@ def checkForKeyPress():
 DrawCenterMessage("Push The Button", wid2, high2, 70)
 
 while True:
-  if GPIO.input(SWITCH) == False:
-    snap = 0
-    pose_number = snap+1
-    for i in range(5):
-      GPIO.output(READY_LED, False)
-      time.sleep(0.5)
-      GPIO.output(READY_LED, True)
-      time.sleep(0.5)
-    GPIO.output(READY_LED, False)
-    while snap < 4:
-      DrawCenterMessage(poser[snap],wid2,high2+100,70)
-      print("pose number %d" % pose_number)
-      for i in range(5):
-        GPIO.output(POSE_LED, False)
-        time.sleep(0.5)
-        GPIO.output(POSE_LED, True)
-        time.sleep(0.5)
-      for i in range(10):
-        GPIO.output(POSE_LED, False)
-        time.sleep(0.1)
-        GPIO.output(POSE_LED, True)
-        time.sleep(0.1)
-      time.sleep(0.7)
-      GPIO.output(POSE_LED, True)
-
-      #Temp
-      
-      terminate("Ended at the end!")
+    if GPIO.input(SWITCH) == False:
+        snap = 0
+        # TODO: Work on screen layout
+        DrawCenterMessage("Get Ready" ,wid2,high2+100,70)
+        for i in range(5):
+            GPIO.output(READY_LED, False)
+            time.sleep(0.5)
+            GPIO.output(READY_LED, True)
+            time.sleep(0.5)
+        GPIO.output(READY_LED, False)
     
+        while snap < 4:
+            pose_number = snap+1
+            DrawCenterMessage(poser[snap],wid2,high2+100,70)
+            for i in range(5):
+                GPIO.output(POSE_LED, False)
+                time.sleep(0.5)
+                GPIO.output(POSE_LED, True)
+                time.sleep(0.5)
+        
+            for i in range(5):
+                GPIO.output(POSE_LED, False)
+                time.sleep(0.5)
+                GPIO.output(POSE_LED, True)
+                time.sleep(0.5)
+                countdown = str(5-i)
+                BigNumber(countdown)
+            time.sleep(1)
+            # TODO: Work on screen layout
+            DrawCenterMessage("Snap" ,wid2,high2+100,100)
+            # Takes a photo with connected DSLR
+            print("pose number %d" % pose_number)
+            GPIO.output(POSE_LED, False)
+            gpout = subprocess.check_output("gphoto2 --capture-image-and-download --filename /home/pi/photobooth_images/photobooth%Y%m%d%H%M%S.jpg", stderr=subprocess.STDOUT, shell=True)
+            print(gpout)
+            if "ERROR" not in gpout:
+                snap += 1
+        # Create photo strip and send it to the printer
+        # TODO: Work on screen layout
+        DrawCenterMessage("Assembling" ,wid2,high2+100,100)
+        print("Assembling the photo strip")
+        GPIO.output(PRINT_LED, True)
+        subprocess.call("sudo /home/pi/scripts/photobooth_tester/assemble_and_print", shell=True)
+
+        # TODO: Work on screen layout
+        DrawCenterMessage("Printing" ,wid2,high2+100,100)
+        print("please wait while your photos print...")
+        
+        # TODO: determine amount of time to compile the montage, and if printing the photo how long that will take
+        # TODO: check status of printer instead of using this arbitrary wait time
+        time.sleep(10)
+        GPIO.output(PRINT_LED, False)
+        print("ready for next round")
+        GPIO.output(READY_LED, True)
+
+        # Temp
+        terminate("Ended at the end!")
+        
