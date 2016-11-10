0b2c230fa70eca31e3418afe03085ae6f43708df
muse435 <muse435@gmail.com>
2016-11-09 21:54:43 -0500

    Update imgtest.py

--- imgtest.py
+++ imgtest.py
@@ -118,60 +118,6 @@ while True:
     if GPIO.input(RESET) == False:
         terminate("Killed by Reset Switch")
     if GPIO.input(SWITCH) == False:
-        snap = 0
-        # TODO: Work on screen layout
-        DrawCenterMessage("Get Ready" ,wid2,high2+100,70)
-        for i in range(5):
-            GPIO.output(READY_LED, False)
-            time.sleep(0.5)
-            GPIO.output(READY_LED, True)
-            time.sleep(0.5)
-        GPIO.output(READY_LED, False)
-    
-        while snap < 4:
-            pose_number = snap+1
-            DrawCenterMessage(poser[snap],wid2,high2+100,70)
-            for i in range(5):
-                GPIO.output(POSE_LED, False)
-                time.sleep(0.5)
-                GPIO.output(POSE_LED, True)
-                time.sleep(0.5)
-        
-            for i in range(5):
-                GPIO.output(POSE_LED, False)
-                time.sleep(0.5)
-                GPIO.output(POSE_LED, True)
-                time.sleep(0.5)
-                countdown = str(5-i)
-                BigNumber(countdown)
-            time.sleep(1)
-            # TODO: Work on screen layout
-            DrawCenterMessage("Snap" ,wid2,high2+100,100)
-            # Takes a photo with connected DSLR
-            print("pose number %d" % pose_number)
-            GPIO.output(POSE_LED, False)
-            gpout = subprocess.check_output("gphoto2 --capture-image-and-download --filename /home/pi/photobooth_images/photobooth%Y%m%d%H%M%S.jpg", stderr=subprocess.STDOUT, shell=True)
-            print(gpout)
-            if "ERROR" not in gpout:
-                snap += 1
-        # Create photo strip and send it to the printer
-        # TODO: Work on screen layout
-        DrawCenterMessage("Assembling" ,wid2,high2+100,100)
-        print("Assembling the photo strip")
-        GPIO.output(PRINT_LED, True)
-        subprocess.call("sudo /home/pi/scripts/photobooth_tester/assemble_and_print", shell=True)
-
-        # TODO: Work on screen layout
-        DrawCenterMessage("Printing" ,wid2,high2+100,100)
-        print("please wait while your photos print...")
-        
-        # TODO: determine amount of time to compile the montage, and if printing the photo how long that will take
-        # TODO: check status of printer instead of using this arbitrary wait time
-        time.sleep(10)
-        GPIO.output(PRINT_LED, False)
-        print("ready for next round")
-        GPIO.output(READY_LED, True)
-
-        # Temp
-        #terminate("Ended at the end!")
-        
+        DrawCenterPhoto(1, wid2, high2, 20)
+        time.sleep(20)
+        terminate("End of test")
