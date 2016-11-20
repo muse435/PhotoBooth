5b50dd3e042025df1c1873aa1ffcc4abe525dd46
muse435 <muse435@gmail.com>
2016-11-19 23:53:54 -0500

    Set mouse visibility to false

--- dslr_photo_booth.py
+++ dslr_photo_booth.py
@@ -50,6 +50,7 @@ black = pygame.Color(0,0,0)
 pygame.init()
 pygame.display.init()
 screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+pygame.mouse.set_visible(0)
 
 
 def CountDownScreen(pose, timerNumber):
